406378a87e9d83e7fc9957535489198714e55fb0
Adam Yu Wen <wen.adam@hotmail.com>
2019-07-01 23:03:52 -0400

    Re-fomat tab

--- Chapter06/PE05.cpp
+++ Chapter06/PE05.cpp
@@ -5,35 +5,35 @@ using std::cout;
 using std::endl;
 
 int main() {
-	double input;
+    double input;
 
-	cout << "Enter your income: ";
-	while (cin >> input) {
-		if (input >= 0) {
-			double tax;
-			if (input > 35000) {
-				tax = 10000 * 0.1 + 20000 * 0.15 + (input - 35000) * 0.2;
-			}
-			else if (input > 20000) {
-				tax = 10000 * 0.1 + (input - 20000) * 0.15;
-			}
-			else if (input > 10000) {
-				tax = (input - 10000) * 0.1;
-			}
-			else {
-				tax = 0.0;
-			}
+    cout << "Enter your income: ";
+    while (cin >> input) {
+        if (input >= 0) {
+            double tax;
+            if (input > 35000) {
+                tax = 10000 * 0.1 + 20000 * 0.15 + (input - 35000) * 0.2;
+            }
+            else if (input > 20000) {
+                tax = 10000 * 0.1 + (input - 20000) * 0.15;
+            }
+            else if (input > 10000) {
+                tax = (input - 10000) * 0.1;
+            }
+            else {
+                tax = 0.0;
+            }
 
-			cout << "Your solicit income is " << input << " tvarps.\n";
-			cout << "You would owe " << tax << " tvarps.\n";
-			cout << "Enter another income\n";
-			cout << "(enter a negative number or non-numeric input ";
-			cout << "to terminate): ";
-		}
-		else {
-			break;
-		}
-	}
+            cout << "Your solicit income is " << input << " tvarps.\n";
+            cout << "You would owe " << tax << " tvarps.\n";
+            cout << "Enter another income\n";
+            cout << "(enter a negative number or non-numeric input ";
+            cout << "to terminate): ";
+        }
+        else {
+            break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
