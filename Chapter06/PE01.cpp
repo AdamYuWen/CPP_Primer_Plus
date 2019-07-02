8ba60ad70159edcce6ea32ffd7764c985af671fb
Adam Yu Wen <wen.adam@hotmail.com>
2019-07-01 22:46:49 -0400

    Re-format tab

--- Chapter06/PE01.cpp
+++ Chapter06/PE01.cpp
@@ -6,26 +6,26 @@ using std::cout;
 using std::endl;
 
 int main() {
-	char ch;
-	
-	cout << "Enter from keyboard (using to \"@\" to stop):\n";
+    char ch;
 
-	cin.get(ch);
-	while (ch != '@') {
-		if (!isdigit(ch)) {
-			if (islower(ch)) {
-				cout << char(toupper(ch));
-			}
-			else if (isupper(ch)) {
-				cout << char(tolower(ch));
-			}
-			else {
-				cout << ch;
-			}
-		}
-		cin.get(ch);
-	}
-	cout << endl;
+    cout << "Enter from keyboard (using to \"@\" to stop):\n";
 
-	return 0;
+    cin.get(ch);
+    while (ch != '@') {
+        if (!isdigit(ch)) {
+            if (islower(ch)) {
+                cout << char(toupper(ch));
+            }
+            else if (isupper(ch)) {
+                cout << char(tolower(ch));
+            }
+            else {
+                cout << ch;
+            }
+        }
+        cin.get(ch);
+    }
+    cout << endl;
+
+    return 0;
 }
