f6a2bb8b3efb643a2a094dedbdeaaacea0d58c09
Adam Yu Wen <wen.adam@hotmail.com>
2019-07-01 22:37:22 -0400

    Initial upload for solutions of Chapter 06

---
+++ Chapter06/PE07.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using std::string;
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    string word;
+    int numVowels = 0, numConsonants = 0, numOthers = 0;
+    cout << "Enter words (q to quit): " << endl;
+    while (cin >> word) {
+        if (word == "q") {
+            break;
+        }
+        else if (isalpha(word[0])) {
+            switch (word[0]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                ++numVowels;
+                break;
+            default:
+                ++numConsonants;
+                break;
+            }
+        }
+        else {
+            ++numOthers;
+        }
+    }
+
+    cout << numVowels << " words beginning with vowels\n";
+    cout << numConsonants << " words beginning with consonants\n";
+    cout << numOthers << " others\n";
+
+    return 0;
+}
